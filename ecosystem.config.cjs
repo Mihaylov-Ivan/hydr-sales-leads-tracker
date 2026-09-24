85677d4011c646a2957e34cb6d8ecb544dd83404
Ivan Mihaylov <mihaylovv001@gmail.com>
2026-09-24 09:21:15 +0300

    port 3000 is https

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,12 +4,12 @@ module.exports = {
       name: "hydr-sales-leads-tracker",
       cwd: __dirname,
       script: "node_modules/next/dist/bin/next",
-      args: "start -p 3000",
+      args: "start -p 3001",
       interpreter: "node",
       exec_mode: "fork",
       env: {
         NODE_ENV: "production",
-        PORT: "3000",
+        PORT: "3001",
       },
     },
     {
@@ -20,8 +20,8 @@ module.exports = {
       exec_mode: "fork",
       env: {
         NODE_ENV: "production",
-        HTTPS_PORT: "3443",
-        TARGET_ORIGIN: "http://127.0.0.1:3000",
+        HTTPS_PORT: "3000",
+        TARGET_ORIGIN: "http://127.0.0.1:3001",
       },
     },
   ],
